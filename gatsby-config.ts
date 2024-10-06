5795746294c4fc5441faaecffd7ce9490ccad1d6
hankaBurdik <hburdik@amp.energy>
2024-10-06 19:42:28 +0200

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,61 @@
+import type { GatsbyConfig } from "gatsby";
+const tailwindConfig = require('./tailwind.config.js')
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Stoleta Valka`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  graphqlTypegen: true,
+  plugins: [{
+    resolve: `gatsby-source-contentful`,
+    options: {
+      spaceId: process.env.CONTENTFUL_SPACE_ID,
+      accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    },
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+    },
+    {
+      resolve: 'gatsby-plugin-postcss',
+      options: {
+        postCssPlugins: [
+          require('tailwindcss')(tailwindConfig),
+          require('autoprefixer'),
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-google-gtag',
+    options: {
+      trackingIds: [
+        "GA-TRACKING_ID", // Google Analytics / GA
+        "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+        "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+      ],
+    },
+    },
+    "gatsby-plugin-mdx", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }, {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }]
+};
+
+export default config;
