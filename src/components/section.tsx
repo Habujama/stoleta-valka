4d62071a40ee0a9bfdb0c4cd7801ac9de9105b40
hankaBurdik <hburdik@amp.energy>
2024-12-08 23:04:49 +0100

    fix mobile

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -15,7 +15,9 @@ const Section = ({
 }: SectionProps) => (
   <div className='mt-12 mb-6'>
     {subtitle && (
-      <h4 className='font-serifSmall text-5xl text-center'>{subtitle}</h4>
+      <h4 className='font-serifSmall text-2xl md:text-5xl text-center'>
+        {subtitle}
+      </h4>
     )}
     <div className='grid grid-rows-2 md:grid-rows-1 md:grid-cols-6 gap-y-4 md:gap-x-4 md:gap-y-0 text-center items-center md:text-left pb-4'>
       <div className={imageRight ? 'md:col-span-4' : 'col-span-2'}>
