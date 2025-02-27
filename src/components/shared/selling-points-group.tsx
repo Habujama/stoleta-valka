08f0d6f1540601264b5d053ab7db71491e18617b
hankaBurdik <hburdik@amp.energy>
2025-02-27 23:35:40 +0100

    add monsters

--- src/components/shared/selling-points-group.tsx
+++ src/components/shared/selling-points-group.tsx
@@ -9,6 +9,7 @@ const { screens } = theme;
 
 const SellingPointsGroup = () => {
   const isMobile = useMedia(`(max-width: ${screens.sm})`);
+  const isTablet = useMedia(`(max-width: ${screens.lg})`);
   return (
     <div className='bg-neutral-800 bg-blend-screen bg-opacity-15 px-12 pb-16 mb-10'>
       <div
@@ -23,53 +24,89 @@ const SellingPointsGroup = () => {
       <ul className='grid grid-rows-4 gap-y-6 md:gap-y-0 max-w-4xl mx-auto md:grid-rows-1 md:grid-cols-4 md:gap-x-6 justify-items-center items-baseline'>
         <SellingPoint
           text='Vyjednávejte, intrikujte, utvářejte a&nbsp;ničte spojenectví.'
-          title='diplomacie'
+          title={isTablet ? '' : 'diplomacie'}
           staticImage={
-            <StaticImage
-              src='../../assets/diplomacie.png'
-              alt='Vyzkoušejte si středověkou diplomacii na vlastní kůži!'
-              className='bg-contain mb-4'
-              placeholder='blurred'
-            />
+            isTablet ? (
+              <StaticImage
+                src='../../assets/diplomacie-mobile.png'
+                alt='Vyzkoušejte si středověkou diplomacii na vlastní kůži!'
+                className='bg-contain mb-4'
+                placeholder='blurred'
+              />
+            ) : (
+              <StaticImage
+                src='../../assets/diplomacie.png'
+                alt='Vyzkoušejte si středověkou diplomacii na vlastní kůži!'
+                className='bg-contain mb-4'
+                placeholder='blurred'
+              />
+            )
           }
         />
         <SellingPoint
           text='Čelte cizím agentům, přepadejte cizí lodě, braňte svá města,
           nechte o&nbsp;sobě nebo o&nbsp;svém nepříteli skládat básně
           a&nbsp;hlavně - prožijte si&nbsp;dopady svých činů.'
-          title='konflikty'
+          title={isTablet ? '' : 'konflikty'}
           staticImage={
-            <StaticImage
-              src='../../assets/konflikty.png'
-              alt='konflikty'
-              className='bg-contain mb-4'
-              placeholder='blurred'
-            />
+            isTablet ? (
+              <StaticImage
+                src='../../assets/konflikty-mobile.png'
+                alt='konflikty'
+                className='bg-contain mb-4'
+                placeholder='blurred'
+              />
+            ) : (
+              <StaticImage
+                src='../../assets/konflikty.png'
+                alt='konflikty'
+                className='bg-contain mb-4'
+                placeholder='blurred'
+              />
+            )
           }
         />
         <SellingPoint
           text='Veďte své armády k&nbsp;vítězství... nebo k&nbsp;záhubě.'
-          title='strategie'
+          title={isTablet ? '' : 'strategie'}
           staticImage={
-            <StaticImage
-              src='../../assets/valecna-strategie.png'
-              alt='strategie'
-              className='bg-contain mb-4'
-              placeholder='blurred'
-            />
+            isTablet ? (
+              <StaticImage
+                src='../../assets/strategie-mobile.png'
+                alt='strategie'
+                className='bg-contain mb-4'
+                placeholder='blurred'
+              />
+            ) : (
+              <StaticImage
+                src='../../assets/valecna-strategie.png'
+                alt='strategie'
+                className='bg-contain mb-4'
+                placeholder='blurred'
+              />
+            )
           }
         />
         <SellingPoint
-          text='27 mužských a 12 ženských postav, z nichž každá má vlastní
-          propracovaný příběh a možnost ovlivnit svůj osud.'
-          title='výběr z 39 rolí'
+          text='27&nbsp;mužských a&nbsp;12&nbsp;ženských postav, z&nbsp;nichž každá má vlastní
+          propracovaný příběh a&nbsp;možnost ovlivnit svůj osud.'
+          title={isTablet ? '' : 'výběr z 39 rolí'}
           staticImage={
-            <StaticImage
-              src='../../assets/vyber-roli.png'
-              alt='výběr z 39 rolí'
-              className='bg-contain mb-4'
-              placeholder='blurred'
-            />
+            isTablet ? (
+              <StaticImage
+                src='../../assets/vyber-roli-mobile.png'
+                alt='výběr z 39 rolí'
+                className='bg-contain mb-4'
+                placeholder='blurred'
+              />
+            ) : (
+              <StaticImage
+                src='../../assets/vyber-roli.png'
+                alt='výběr z 39 rolí'
+                className='bg-contain mb-4'
+                placeholder='blurred'
+              />
+            )
           }
         />
       </ul>
