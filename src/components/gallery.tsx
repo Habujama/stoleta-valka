4448fc35a32a58bbc8416f093e89f80c071d0cbf
hankaBurdik <hburdik@amp.energy>
2025-03-06 21:12:27 +0100

    add buttons for gallery details scroll

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -6,6 +6,11 @@ import theme from 'tailwindcss/defaultTheme';
 
 const { screens } = theme;
 
+interface ImageProps {
+  image: IGatsbyImageData;
+  order: number;
+}
+
 const Gallery = () => {
   const { contentfulFotogalerie } = useStaticQuery(graphql`
         query GalleryQuery {
@@ -19,26 +24,30 @@ const Gallery = () => {
         }
       `);
 
-  const [selectedImage, setSelectedImage] = useState<IGatsbyImageData | null>(
-    null,
-  );
+  const [selectedImage, setSelectedImage] = useState<ImageProps | null>(null);
   const isMobile = useMedia(`(max-width: ${screens.lg})`);
 
   return (
     <>
       <div className='grid grid-flow-col auto-cols-max gap-1 w-full whitespace-nowrap overflow-x-scroll no-scrollbar p-4 bg-neutral-800 bg-blend-screen bg-opacity-15'>
         {contentfulFotogalerie.fotky.map(
-          (photo: {
-            small: { gatsbyImageData: IGatsbyImageData };
-            large: IGatsbyImageData;
-            title: string;
-          }) => {
+          (
+            photo: {
+              small: { gatsbyImageData: IGatsbyImageData };
+              large: IGatsbyImageData;
+              title: string;
+            },
+            index: number,
+          ) => {
             const image = getImage(photo.small);
             return (
               <div
                 key={photo.title}
                 onClick={() =>
-                  !isMobile && setSelectedImage(photo.large || null)
+                  !isMobile &&
+                  setSelectedImage(
+                    photo.large ? { image: photo.large, order: index } : null,
+                  )
                 }
               >
                 {image && (
@@ -61,16 +70,51 @@ const Gallery = () => {
         >
           <div className='relative p-44'>
             <button
-              className='absolute top-20 right-20 text-white text-3xl font-bold'
+              className='absolute left-10 top-1/2 transform -translate-y-1/2 text-white text-3xl md:text-5xl xl:text-7xl font-bold'
+              onClick={(e) => {
+                e.stopPropagation();
+                if (selectedImage.order > 0) {
+                  const prevImage =
+                    contentfulFotogalerie.fotky[selectedImage.order - 1].large;
+                  setSelectedImage({
+                    image: prevImage,
+                    order: selectedImage.order - 1,
+                  });
+                }
+              }}
+            >
+              {'<'}
+            </button>
+            <button
+              className='absolute top-20 right-20 text-white text-3xl md:text-5xl xl:text-7xl font-bold'
               onClick={() => setSelectedImage(null)}
             >
               &times;
             </button>
             <GatsbyImage
-              image={selectedImage}
+              image={selectedImage.image}
               alt=''
               className='max-w-full max-h-full object-contain'
             />
+            <button
+              className='absolute right-10 top-1/2 transform -translate-y-1/2 text-white text-3xl md:text-5xl xl:text-7xl font-bold'
+              onClick={(e) => {
+                e.stopPropagation();
+                if (
+                  selectedImage.order <
+                  contentfulFotogalerie.fotky.length - 1
+                ) {
+                  const nextImage =
+                    contentfulFotogalerie.fotky[selectedImage.order + 1].large;
+                  setSelectedImage({
+                    image: nextImage,
+                    order: selectedImage.order + 1,
+                  });
+                }
+              }}
+            >
+              {'>'}
+            </button>
           </div>
         </div>
       )}
